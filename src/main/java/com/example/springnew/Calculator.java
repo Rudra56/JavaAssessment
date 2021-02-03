d4d66c5e2ff811e58c7fef0c686da97a197df784
Madhu Thaduri <madhubabu.thaduri@gmail.com>
2021-02-03 11:46:38 -0500

    minor changes for" enter numeric digits"

--- src/main/java/com/example/springnew/Calculator.java
+++ src/main/java/com/example/springnew/Calculator.java
@@ -1,10 +1,14 @@
 package com.example.springnew;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Calculator implements ICalculator{
 
-    public int add(String... numbers) {
+    public int add(String... numbers)
+    {
 
         int result = 0;
         for(String number : numbers) {
@@ -30,6 +34,18 @@ public class Calculator implements ICalculator{
         return result;
     }
 
+    public static boolean isNumber(String... input){
+
+        for(String s : input){
+            for(int i=0;i<s.length();i++){
+                if(!Character.isDigit(s.charAt(i))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int result = 0;
 
@@ -37,31 +53,37 @@ public class Calculator implements ICalculator{
         String operation;
 
         Calculator calculator = new Calculator();
-
         Scanner scanObject = new Scanner(System.in);
-
         System.out.println("please enter numbers seperated by space");
-
         inputNumber =scanObject.nextLine();
-        String[] numbers = inputNumber.split("\\s");
-        System.out.println("what operation");
-        operation = scanObject.next();
-        scanObject.close();
+            String[] numbers = inputNumber.split("\\s");
+            if(isNumber(numbers))
+            {
+            System.out.println("what operation");
+            operation = scanObject.next();
+            scanObject.close();
+            switch (operation) {
 
-        switch (operation) {
+                case "add":
+                    result = calculator.add(numbers);
+                    break;
 
-            case "add" : result = calculator.add(numbers);
-                break;
+                case "substract":
+                    result = calculator.substract(numbers);
+                    break;
 
-            case "substract" : result = calculator.substract(numbers);
-                break;
-
-            case "multiply" : result = calculator.multiply(numbers);
-                break;
+                case "multiply":
+                    result = calculator.multiply(numbers);
+                    break;
 
 
+            }
+
+            System.out.println(result);
         }
-        System.out.println(result);
+        else {
+            System.out.println("enter a numeric number instead of an alphabet");
+        }
     }
 
 
